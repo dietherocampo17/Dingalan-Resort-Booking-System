b0162a8adbc97b1cd866335230bf59f0310bef58
Diether Ocampo <cr.diether17@gmail.com>
2026-01-05 13:35:27 +0800

    Updates

--- assets/swipe-back-legacy-BjlQ2PSi.js
+++ assets/swipe-back-legacy-BjlQ2PSi.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BLaHj34x.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BQ7xMF9x.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
