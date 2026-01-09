31707f7fae0982491774e3edfb571f1ad4942426
Diether Ocampo <cr.diether17@gmail.com>
2026-01-09 08:04:28 +0800

    Updates

--- assets/ios.transition-legacy-B9lvJO92.js
+++ assets/ios.transition-legacy-B9lvJO92.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-reyJedgD.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-WHGMeq6i.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
