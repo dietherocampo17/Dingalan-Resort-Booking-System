af939abe36cecdfbdbf3d338cf8f57fe3d51ff5c
Diether Ocampo <cr.diether17@gmail.com>
2026-01-09 09:15:56 +0800

    Updates

--- assets/md.transition-legacy-DCQWrlB6.js
+++ assets/md.transition-legacy-DCQWrlB6.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-WHGMeq6i.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-CAz-GeVa.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
