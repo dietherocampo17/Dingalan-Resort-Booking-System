ba98dc25769cbe32e297f5f10ef92ca90bb8f471
Diether Ocampo <cr.diether17@gmail.com>
2026-01-12 09:13:56 +0800

    Updates

--- assets/ios.transition-legacy-BZflALGO.js
+++ assets/ios.transition-legacy-BZflALGO.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DrvIMC3D.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-B0qM-Blb.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
